840c32212cb5d5c581c07a5d7ef9c856d9eeab2e
ngodingbentar <dewaqintoro@gmail.com>
2022-03-10 20:24:36 +0700

    login

--- webSocketClient/src/index.js
+++ webSocketClient/src/index.js
@@ -1,9 +1,20 @@
-import ReactDOM from "react-dom"
-import react, { Component } from "react"
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Card, Avatar, Input, Typography } from 'antd';
+import 'antd/dist/antd.css';
+import './index.css'
+
+const { Search } = Input;
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 export default class App extends Component {
+
+  state ={
+    userName: '',
+    isLoggedIn: false,
+  }
+
   onButtonClicked = (value) => {
     client.send(JSON.stringify({
       type: "message",
@@ -21,8 +32,20 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
-        <button onClick={() => this.onButtonClicked("hello dew")}>Send Message</button>
+      <div className='main'>
+        { this.state.isLoggedIn
+        ?
+          <button onClick={() => this.onButtonClicked("hello dew")}>Send Message</button>
+        :
+          <div style={{ padding: '200px 40px' }}>
+            <Search
+              placeholder="Enter Username"
+              enterButton="Login"
+              size="large"
+              onSearch={value => this.setState({ isLoggedIn: true, userName: value })}
+            />
+          </div>
+        }
       </div>
     )
   }
