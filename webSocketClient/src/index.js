6d66f8b47df3747caab52a21960c2874b6e8be8f
ngodingbentar <dewaqintoro@gmail.com>
2022-03-10 20:17:03 +0700

    send data to server

--- webSocketClient/src/index.js
+++ webSocketClient/src/index.js
@@ -4,15 +4,25 @@ import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 export default class App extends Component {
+  onButtonClicked = (value) => {
+    client.send(JSON.stringify({
+      type: "message",
+      msg: value,
+    }));
+  }
   componentDidMount(){
     client.onopen = () => {
       console.log('websocket client connected')
+    }
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data)
+      console.log('got reply! ', dataFromServer);
     }
   }
   render() {
     return (
       <div>
-        Testing App
+        <button onClick={() => this.onButtonClicked("hello dew")}>Send Message</button>
       </div>
     )
   }
