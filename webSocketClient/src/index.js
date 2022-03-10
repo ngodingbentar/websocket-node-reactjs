e3bbb20d567314b80c244f1a4b5e9b6f07b38612
ngodingbentar <dewaqintoro@gmail.com>
2022-03-10 20:11:20 +0700

    connected to server BE

--- webSocketClient/src/index.js
+++ webSocketClient/src/index.js
@@ -1,7 +1,14 @@
 import ReactDOM from "react-dom"
 import react, { Component } from "react"
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 export default class App extends Component {
+  componentDidMount(){
+    client.onopen = () => {
+      console.log('websocket client connected')
+    }
+  }
   render() {
     return (
       <div>
